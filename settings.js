0b273f138496da1a76528d2d36dfc6b4b33b5bcd
alommamun055-star <alommamun055@gmail.com>
2026-05-17 22:01:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20, 
-  storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+packname: 'RELAX-MD',
+author: 'RELAX',
+botName: "RELAX-MD",
+botOwner: 'RELAX',
+ownerNumber: '918116539293',
+giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+commandMode: "public",
+maxStoreMessages: 20,
+storeWriteInterval: 10000,
+description: "RELAX-MD WhatsApp Bot",
+version: "3.0.7",
+updateZipUrl: "https://github.com/alommamun055-star/RELAX-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
